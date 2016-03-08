0478e244fa3dd622923cdebf9fc8d344dfc0094a
I-Shiun Kuo <ishiunkuo@icloud.com>
2016-03-07 23:04:28 -0500

    Initial Commit

---
+++ Instagrams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagrams
+//
+//  Created by I-Shiun Kuo on 3/7/16.
+//  Copyright © 2016 I-Shiun Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
